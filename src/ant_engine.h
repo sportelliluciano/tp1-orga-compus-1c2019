a6f810e34f383e5380e5afa838385e40ccba0df7
Lucho <sportelliluciano@gmail.com>
2019-04-14 16:11:45 -0300

    Base TP1

---
+++ src/ant_engine.h
@@ -0,0 +1,17 @@
+#ifndef __ANT_ENGINE_H__
+#define __ANT_ENGINE_H__
+
+#include <stdio.h>
+#include <unistd.h>
+
+void* paint(void *ant, void *grid, void *palette, void *rules,  uint32_t iterations);
+
+#define panic(s)       \
+  do {                 \
+    fprintf(stderr, "%s: %s\n", __FUNCTION__, s);\
+    exit(1);            \
+  } while(0);
+
+
+#endif /* __ANT_ENGINE_H__ */
+
