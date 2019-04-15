13941bec1c7a494038d6f33e992fb26b63d82f74
Lucho <sportelliluciano@gmail.com>
2019-04-15 01:44:23 -0300

    Agregada version basica del TP (en C)

--- src/ant_engine.c
+++ src/ant_engine.c
@@ -3,26 +3,118 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
+#include "artist_ant.h"
+
+typedef uint32_t uintvector_t;
+
+// Gets the length of a uintvector_t*
+#define UINTV_LEN(intv) (*(intv - 1))
+
+
+static void make_rules_map(int rules_map[256], uintvector_t* palette, uintvector_t *rules) {
+  for (uint32_t i=0; i<UINTV_LEN(palette); i++) {
+    rules_map[palette[i]] = rules[i];
+  }
+}
 
 void*
 paint(void *ant, void *grid, void *palette, void *rules,  uint32_t iterations)
 {
-  panic ("Implement me!");
+  ant_t *sant = ant;
+  square_grid_t *sgrid = grid;
+
+  orientation_t orientation[] = {ON, OE, OS, OW};
+  int current_orientation = 0;
+
+  int increments[4][2] = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
+
+  uintvector_t *colours = palette;
+  uint32_t colour_idx = 1;
+
+  int rules_map[256];
+  make_rules_map(rules_map, palette, rules);
+  
+  for (uint32_t i = 0; i < iterations; i++, colour_idx++) {
+    if (colour_idx >= UINTV_LEN(colours))
+      colour_idx = 0;
+    
+    colour_t current_colour = colours[colour_idx];
+
+    // Change orientation
+    current_orientation = (current_orientation + rules_map[current_colour]) % 4;
+    sant->o = orientation[current_orientation];
+    
+    // Paint the cell
+    sgrid->grid[sant->y][sant->x] = current_colour;
+    
+    // Move to next cell
+    sant->x += increments[current_orientation][0];
+    sant->y += increments[current_orientation][1];
+    
+    if (sant->x > sgrid->width) // Check boundaries
+      sant->x = 0;
+    else if (sant->x == (uint32_t)-1) // sant->x is unsigned > check overflow
+      sant->x = sgrid->width - 1;
+    if (sant->y > sgrid->height)
+      sant->y = 0;
+    else if (sant->y == (uint32_t)-1)
+      sant->y = sgrid->height - 1;
+  }
+
   return grid;
+}
+
+static uintvector_t* str_to_uintvector(char *str) {
+  uint32_t n_chars = 0, n_data = 0;
+  while (str[n_chars]) {
+    if (str[n_chars] != '|')
+      n_data++;
+    n_chars++;
+  }
+
+  // sizeof(*vector) es una desreferencia mágica. Thanks god se resuelve
+  //  en tiempo de compilación!
+  uintvector_t *vector = malloc(sizeof(*vector) * (n_data + 1));
+  if (!vector)
+    return NULL;
+
+  vector[0] = n_data;
+
+  for (uint32_t i = 0, j = 1; i < n_chars; i++) {
+    if (str[i] != '|')
+      vector[j++] = str[i];
+  }
+
+  return vector + 1;
 }
 
 void*
 make_rules(char *spec)
 {
-  panic("Implement me!");
-  return NULL;
+  uintvector_t* rules = str_to_uintvector(spec);
+  for (uint32_t i=0;i<UINTV_LEN(rules);i++) {
+    rules[i] = (rules[i] == 'L') ? 1:3;
+  }
+  return rules;
+}
+
+static colour_t
+get_colour(char c)
+{
+  static char * index = "RGBYNW";
+  char *p = strchr(index, c);
+  return (p - index);
 }
 
 void*
 make_palette(char *colours)
 {
-  panic("Implement me!");
-  return NULL;
+  uintvector_t *palette = str_to_uintvector(colours);
+  for (uint32_t i=0;i<UINTV_LEN(palette);i++) {
+    palette[i] = get_colour(palette[i]);
+  }
+  return palette;
 }
 
